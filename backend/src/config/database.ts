3b266cbdd79b58d900599386b8a02ff9dc7a3f00
PATHAK <pathaktirath2000@gmail.com>
2025-06-28 13:43:15 +0530

    configured database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL as string);
+    await mongoose.connect(process.env.MONGO_URI as string);
     console.log("Connected successfully to the database");
   } catch (err) {
     console.log(err);
