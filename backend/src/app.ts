368a590e8fb6319ba8fd27dab471181b52e78e60
PATHAK <pathaktirath2000@gmail.com>
2025-07-27 13:12:05 +0530

    Added the routers and seperation of concerns applied

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,20 @@ import "dotenv/config";
 import express from "express";
 import { connectDB } from "./config/database";
 import cookieParser from "cookie-parser";
+import { authRouter } from "./routes/auth";
+import { profileRouter } from "./routes/profile";
+import { connectionRequestRouter } from "./routes/connectionRequest";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/",authRouter)
+app.use("/",profileRouter)
+app.use("/",connectionRequestRouter)
+
+
 
 app.listen(3000, () => {
   connectDB()
